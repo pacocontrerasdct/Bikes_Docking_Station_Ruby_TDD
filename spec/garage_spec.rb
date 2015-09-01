7dd9ed767cf451e127c99ab07ccfcd6fb597b3b3
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-01 22:09:24 +0100

    Garage now is really is able to admit bikes

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,16 +1,19 @@
 require_relative './spec_helper'
 
 describe Garage do
-
+  let(:garage) { Garage.new }
+  
   it 'should be empty at the beginning' do
-    garage = Garage.new
     expect(garage.bikes_count).to eq 0
   end
 
-  it 'should be able to admit a bike' do
-    garage = Garage.new
+  it 'should be able to admit a broken bike' do
     bike = Bike.new
-    garage.dock bike
+
+    broken_bike = bike
+    broken_bike.break
+
+    garage.dock bike if broken_bike.broken? == true
 
     expect(garage.bikes_count).to eq 1
   end
