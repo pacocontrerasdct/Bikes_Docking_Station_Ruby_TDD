1c10b4b5f5b5fe0c5067deaf6bcc9e0a4a6a93bf
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-01 22:01:55 +0100

    Garage is empty at the beginning and is able to admit bikes

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,4 +1,15 @@
 class Garage
 
+  def initialize
+    @bikes = []
+  end
+
+  def bikes_count
+    @bikes.count
+  end
+
+  def dock bike
+    @bikes << bike
+  end
 
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,6 +2,18 @@ require_relative './spec_helper'
 
 describe Garage do
 
+  it 'should be empty at the beginning' do
+    garage = Garage.new
+    expect(garage.bikes_count).to eq 0
+  end
+
+  it 'should be able to admit a bike' do
+    garage = Garage.new
+    bike = Bike.new
+    garage.dock bike
+
+    expect(garage.bikes_count).to eq 1
+  end
 
 
 
