6cb274eb618cf7ae6d876dbb585ab49560137d83
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-01 23:26:23 +0100

    van and van_spec files refactored

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,31 +2,27 @@ require_relative './spec_helper'
 
 describe Garage do
   let(:garage) { Garage.new }
+  let(:bike) { Bike.new }
+  let(:van) { Van.new }
   
   it 'should be empty at the beginning' do
     expect(garage.bikes_count).to eq 0
   end
 
-  it 'should be able to admit a broken bike' do
-    bike = Bike.new
-
+  it 'should be able to admit a broken bike and download it from the van' do
     broken_bike = bike
     broken_bike.break
-
     garage.dock bike if broken_bike.broken? == true
-
+    van.download bike
+    
     expect(garage.bikes_count).to eq 1
   end
 
-  it 'should be able to fix a broken bike and load into the van' do
-    bike = Bike.new
-    van = Van.new
-
+  it 'should be able to fix a broken bike and load it into the van' do
     broken_bike = bike
     broken_bike.fix
     van.load bike if broken_bike.broken? == false
-
-    garage.release bike if van.load bike
+    garage.release bike
 
     expect(garage.bikes_count).to eq 0
   end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,22 +4,25 @@ describe Van do
 
   let(:van) { Van.new({capacity: 5}) }
   let(:bike) { Bike.new }
+  let(:garage) { Garage.new }
+  let(:station) { DockingStation.new }
   
   def fill_van van
     5.times { van.load(Bike.new) }
   end
 
   it 'should be empty at the begining' do
-
     expect(van.bikes_in_the_van_count).to eq 0
   end
 
+  # This Van Loads bikes both at Docking Station and at Garage
   it 'should be able to load bikes' do
     van.load(bike)
 
     expect(van.bikes_in_the_van_count).to eq 1
   end
 
+  # This Van Downloads bikes both at Docking Station and at Garage
   it 'should be able to download bikes' do
     van.download(bike)
 
@@ -32,25 +35,26 @@ describe Van do
     expect(van.full_capacity?).to be true
   end
 
+  # When load broken bikes count+1 in Van counter and count-1 in Docking counter
   it 'should load only broken bikes at docking station' do
     broken_bike = bike
     broken_bike.break
 
     van.load bike if broken_bike.broken? == true
-
+    station.release bike
+    
     expect(van.bikes_in_the_van_count).to eq 1
-
   end
 
+  # When load fixed bikes count+1 in Van counter and count-1 in Garage counter
   it 'should load only fixed bikes at Garage' do
     fixed_bike = bike
     fixed_bike.fix
 
     van.load bike if fixed_bike.broken? == false
+    garage.release bike
 
     expect(van.bikes_in_the_van_count).to eq 1
   end
-
-
 
 end
